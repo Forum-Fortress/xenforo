ab76b1fdcbbf13551f12617e031d8f3c9d45757a
Forum Fortress Dev <dev@forumfortress.local>
2026-09-08 00:20:49 +0100

    Limit public tree to installable plugin source

--- tests/run_ff_api_resilience_contract.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require_once dirname(__DIR__) . '/upload/src/addons/ForumFortress/Protect/Service/FfApiResilience.php';
-require_once __DIR__ . '/FfApiResilienceContract.php';
-
-exit(FfApiResilienceContract::run('FfApiResilience'));
